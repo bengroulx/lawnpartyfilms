afd932f9a7a3476ab99d550a93ba5a167e638004
Ben Groulx <ben@bengroulx.com>
2018-07-10 19:10:44 -0700

    Update /film projects to use new autoplaying video feature

--- site/templates/video.php
+++ site/templates/video.php
@@ -1,109 +1,144 @@
 <?php snippet('head') ?>
 
-<header class="c-section c--large u-align-center">
-    <h1 class="u-regular" style="margin-bottom:1rem;"><?php echo $page->title()->html() ?></h1>
-    <h2 class="u-regular u-mono u-small-text u-uppercase u-tracked-slightly u-grey"><?php echo $page->artist()->html() ?></h2>
+
+<header class="c-section c--large a-fade-in">
+    <div class="c-contain">
+        <h1 class="u-regular" style="margin-bottom:1rem;"><?php echo $page->title()->html() ?></h1>
+        <h2 class="u-regular u-uppercase u-small-text u-uppercase u-tracked-slightly u-grey"><?php echo $page->artist()->html() ?></h2>
+    </div>
 </header>
 
-<section class="c-section c--no-padding" role="main">
-    <div class="c-contain">
 
-        <?php if($video = $page->VideolinkA()): ?>
-        <div class="c-ratio c--16x9 u-gradient u-clip u-bg-grey u-bg-center u-bg-cover u-width-full js-lazy"
-          data-src="<?php echo $page->image()->url() ?>"><iframe src="https://www.youtube.com/embed/<?php echo $video ?>"></iframe>
+<section class="c-section c--no-padding a-fade-in">
+    <div class="+c-contain">
 
-            <div class="u-position u-left-0 u-bottom-0 u-right-0" style="margin:1rem; z-index:10;">
-                <figcaption class="u-position u-bottom-0 u-left-0 u-mono u-regular u-small-text u-white" style="line-height:1;">
-                    Watch “<?php echo $page->title()->html() ?>”
-                </figcaption>
-                <div class="c-play u-context u-float-end u-white u-text-clip">
-                    Play This Video
+        <?php if($page->VideoLinkYouTube()->isNotEmpty()): ?>
+
+            <div class="c-ratio c--16x9 u-context u-clip u-bg-center u-bg-cover u-width-full js-lazy">
+
+                <div id="js-film-reel">
+                    <iframe class="a-fade-in" style="z-index:1001;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media"
+                        src="https://youtube.com/embed/<?php echo $page->VideoLinkYouTube() ?>?rel=0&amp;showinfo=0&amp;autoplay=1&amp;mute=1&amp;hd=1&amp;&iv_load_policy=3&amp;controls=0">
+                    </iframe>
                 </div>
+
             </div>
 
-</div>
+        <?php elseif($page->VideolinkVimeo()->isNotEmpty()): ?>
 
-<?php elseif($video = $page->VideolinkB()): ?>
-<div class="c-ratio c--16x9 u-gradient u-clip u-bg-grey u-bg-center u-bg-cover u-width-full js-lazy"
-    data-src="<?php echo $page->image()->url() ?>"><iframe src="https://vimeo.com/<?php echo $video ?>"></iframe>
+            <div class="c-ratio c--16x9 u-context u-clip u-bg-center u-bg-cover u-width-full js-lazy">
 
-      <div class="u-position u-left-0 u-bottom-0 u-right-0" style="margin:1rem; z-index:10;">
-          <figcaption class="u-position u-bottom-0 u-left-0 u-mono u-regular u-small-text u-white" style="line-height:1;">
-              Watch “<?php echo $page->title()->html() ?>”
-          </figcaption>
-          <div class="c-play u-context u-float-end u-white u-text-clip">
-              Play This Video
-          </div>
-      </div>
+                <div id="js-film-reel">
+                    <!-- Filled in with a video from the script below-->
+                </div>
 
-  </div>
+                <script>
+                  <?php readfile('assets/js/player-vimeo.js'); ?>
+
+                  var options = {
+                    id: <?php echo $page->VideolinkVimeo() ?>,
+                    color: '999',
+                    autoplay: true,
+                    muted: true,
+                    loop: true,
+                  };
+
+                  var player = new Vimeo.Player('js-film-reel', options);
+                </script>
+
+            </div>
 
         <?php else: ?>
 
-        <figure class="c-ratio c--16x9 u-gradient u-clip u-bg-grey u-bg-center u-bg-cover u-width-full js-lazy"
-        data-src="<?php echo $page->image()->url() ?>">
-        </figure>
+            <div class="c-ratio c--16x9 u-context u-clip u-bg-center u-bg-cover u-width-full js-lazy"
+              <?php if($page->coverImage()->isNotEmpty()): ?>
+                  data-src="/content/<?= $page->diruri() ?>/<?php echo $page->coverImage()?>"
+              <?php else: ?>
+                  data-src="<?php echo $page->image()->url() ?>"
+              <?php endif ?>
+            >
+            </div>
 
         <?php endif ?>
 
     </div>
 </section>
 
+
 <section class="c-section">
     <div class="c-contain">
 
-        <div class="c-grid">
+        <div class="u-flexbox--grande-up" style="justify-content:space-between;">
 
-            <div class="c-text-content" style="margin-right:2rem; white-space:nowrap;">
+            <div class="c-text-content" style="padding-top:0.5rem; margin-right:5rem; margin-bottom:calc(10vh); white-space:nowrap;">
                 <div>
-                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly">Film Type</h3>
+                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly u-half-transparent">Film Type</h3>
                     <p><?php echo $page->type() ?></p>
                 </div>
                 <div>
-                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly">Directed By</h3>
+                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly u-half-transparent">Directed By</h3>
                     <p><?php echo $page->director() ?></p>
                 </div>
                 <div>
-                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly">Produced By</h3>
+                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly u-half-transparent">Produced By</h3>
                     <p><?php echo $page->producer() ?></p>
                 </div>
                 <div>
-                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly">Cinematography By</h3>
+                    <h3 class="u-regular u-uppercase u-small-text u-tracked-slightly u-half-transparent">Cinematography By</h3>
                     <p><?php echo $page->cinematographer() ?></p>
                 </div>
             </div>
 
-            <div class="u-mono u-small-text">
-                <?php echo $page->description()->markdown() ?>
+            <div style="max-width:640px;">
+                <?php echo $page->description()->widont() ?>
             </div>
 
         </div>
 
+        <ul class="a-fade-in">
+
+            <?php foreach($page->pictures()->yaml() as $image): ?>
+                <?php if($img = $page->image($image)): ?>
+                    <li>
+                        <img src="<?= $img->url() ?>" alt="<?= $page->title()->html() ?>" style="width:100%; margin-top:25vh;" />
+                    </li>
+                <?php endif ?>
+            <?php endforeach ?>
+
+        </ul>
+
     </div>
 </section>
 
-<section class="c-section c--no-padding u-bg-gradient">
+
+<section class="c-section c--no-padding">
     <div class="c-video-grid u-flexbox">
 
         <?php foreach($page->children() as $subpage): ?>
             <a class="c-video-grid__item u-context u-clip u-bg-cover u-bg-center u-white u-align-center u-flexbox u-align-middle u-justify-middle" href="<?php echo $subpage->url() ?>" style="background-image: url('<?php if($image = $subpage->image()): ?><?php echo $image->url() ?><?php endif ?>');">
+
                 <div class="c-video-grid__text u-context u-z-stack u-block u-white u-uppercase u-tracked-slightly u-thick-text-shadow">
+
                     <h3 class="u-regular">
                         <?php echo $subpage->title()->html() ?>
                     </h3>
-                    <h5 class="u-regular u-mono u-grey">
+
+                    <h5 class="u-regular u-uppercase u-grey">
                         <?php echo $subpage->artist()->html() ?>
                     </h5>
+
                 </div>
+
             </a>
         <?php endforeach ?>
 
     </div>
 </section>
 
+
 <div hidden id="template-<?php echo $page->template() ?>">
     <style><?php readfile('assets/css/templates/videos.css'); ?></style>
-    <script src="<?php echo url('assets/js/magnific.js') ?>"></script>
 </div>
+
 
 <?php snippet('footer') ?>
