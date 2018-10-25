001867a48b684323d84a3027e76907b0ce0725db
Ben Groulx <ben@bengroulx.com>
2018-10-25 12:00:19 -0700

    Restrict layout width of titles on Default template

--- site/snippets/header-title.php
+++ site/snippets/header-title.php
@@ -1,7 +1,9 @@
 <header class="c-section c--large">
+    <div class="c-contain">
 
-    <h1 class="u-regular">
-        <?php echo $page->title()->html() ?>
-    </h1>
+        <h1 class="u-regular">
+            <?php echo $page->title()->html() ?>
+        </h1>
 
+    </div>
 </header>
